ea6398a12cbcc24e646710f092376f9e7c80cb2d
Rodrigo Scomação do Nascimento <rodrigo.sc.na@gmail.com>
2025-09-26 18:15:14 -0300

    ci: adapt build settings for library instead of app

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { storybookTest } from '@storybook/addon-vitest/vitest-plugin'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
+import rollupPluginNodeExternals from 'rollup-plugin-node-externals'
 import { defineConfig } from 'vite'
 
 const dirname =
@@ -15,9 +16,27 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.join(dirname, 'src/index.ts'),
-      fileName: (format) => `index.${format}`,
-      formats: ['cjs', 'es'],
     },
+    rolldownOptions: {
+      output: [
+        {
+          dir: 'dist/cjs',
+          entryFileNames: '[name].cjs',
+          exports: 'auto',
+          format: 'cjs',
+          preserveModules: true,
+          preserveModulesRoot: 'src',
+        },
+        {
+          dir: 'dist',
+          entryFileNames: '[name].js',
+          format: 'es',
+          preserveModules: true,
+          preserveModulesRoot: 'src',
+        },
+      ],
+    },
+    sourcemap: true,
   },
   plugins: [
     react({
@@ -25,6 +44,7 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    rollupPluginNodeExternals(),
   ],
   test: {
     projects: [
